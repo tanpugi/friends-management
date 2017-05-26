c2f31463ee33b00ab3a528abd252d0a201935618
tanpugi <pocoyorocks@gmail.com>
2017-05-27 02:10:42 +0800

    Created a skeleton Spring Boot project.

---
+++ src/main/java/com/tanpugi/fm/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package com.tanpugi.fm;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+	
+}
---
+++ src/main/java/com/tanpugi/fm/ApplicationInitializer.java
@@ -0,0 +1,19 @@
+package com.tanpugi.fm;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+
+/**
+ * An Application helper to input and initialize the application after context is loaded. 
+ * @author jonathan.lonceras@gmail.com
+ */
+@Component
+public class ApplicationInitializer {
+	
+	public void init(Map<String, String> argMap) throws IOException {
+	}
+
+}
