9479b2540205e585d1af8dd5b58cf83d40efe93a
tanpugi <pocoyorocks@gmail.com>
2017-05-29 01:09:33 +0800

    Added empty exception handler

---
+++ src/main/java/com/tanpugi/fm/ApplicationResourceExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.tanpugi.fm;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+@Component
+public class ApplicationResourceExceptionHandler {
+
+}
